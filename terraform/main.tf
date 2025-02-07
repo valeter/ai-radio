b77a1f5bb2785130f4e453e73f7045918a4b9bc4
Ivan Anisimov <ivananisimov2010@gmail.com>
2025-02-07 20:53:12 +0300

    terraform config + base netwok plugin

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-d"
+}
+
+module "yc-vpc" {
+  source              = "github.com/terraform-yc-modules/terraform-yc-vpc.git"
+  network_name        = "ai-radio-network"
+  network_description = "Basic network for ai-radio app"
+  private_subnets = [{
+    name           = "subnet-1"
+    zone           = "ru-central1-a"
+    v4_cidr_blocks = ["10.10.0.0/24"]
+  },
+  {
+    name           = "subnet-3"
+    zone           = "ru-central1-d"
+    v4_cidr_blocks = ["10.12.0.0/24"]
+  }
+  ]
+}
