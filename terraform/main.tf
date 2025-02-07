fa4b5e4426aa08a7de40f3af1fdcf1b0e451a4f5
Ivan Anisimov <ivananisimov2010@gmail.com>
2025-02-07 21:08:10 +0300

    pr terraform check

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,11 @@ module "yc-vpc" {
     v4_cidr_blocks = ["10.10.0.0/24"]
   },
   {
+    name           = "subnet-2"
+    zone           = "ru-central1-b"
+    v4_cidr_blocks = ["10.11.0.0/24"]
+  },
+  {
     name           = "subnet-3"
     zone           = "ru-central1-d"
     v4_cidr_blocks = ["10.12.0.0/24"]
