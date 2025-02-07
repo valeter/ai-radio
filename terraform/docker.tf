106e87574e01749013dc17eb033164d526146608
Ivan Anisimov <ivananisimov2010@gmail.com>
2025-02-07 22:26:57 +0300

    add docker.tf

---
+++ terraform/docker.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-d"
+}
+
+resource "yandex_container_registry" "ai-radio-registry" {
+  name      = "ai-radio-registry"
+  folder_id = "b1g2inpl68al98r9ock7"
+}
+
+resource "yandex_container_registry_iam_binding" "puller" {
+  registry_id = yandex_container_registry.ai-radio-registry
+  role        = "container-registry.images.puller"
+
+  members = [
+    "system:group:organization:bpf7g9i7n7ntnksbdvlr:users",
+  ]
+}
+
+resource "yandex_container_repository" "caster" {
+  name = "${yandex_container_registry.ai-radio-registry.id}/caster"
+}
