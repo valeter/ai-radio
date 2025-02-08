be3843bdcae47e0df1f800e287c82435229238ab
Ivan Anisimov <ivananisimov2010@gmail.com>
2025-02-08 15:59:03 +0300

    more terraform changes

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -1,14 +1,14 @@
 resource "yandex_container_registry" "ai-radio-registry" {
   name      = "ai-radio-registry"
-  folder_id = "b1g2inpl68al98r9ock7"
+  folder_id = var.folder_id
 }
 
 resource "yandex_container_registry_iam_binding" "puller" {
-  registry_id = yandex_container_registry.ai-radio-registry
+  registry_id = yandex_container_registry.ai-radio-registry.id
   role        = "container-registry.images.puller"
 
   members = [
-    "system:group:organization:bpf7g9i7n7ntnksbdvlr:users",
+    "system:group:organization:${var.organization_id}:users",
   ]
 }
 
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "folder_id" {
+  type    = string
+  default = "b1g2inpl68al98r9ock7"
+}
+
+variable "organization_id" {
+  type    = string
+  default = "bpf7g9i7n7ntnksbdvlr"
+}
+
+variable "domain_name" {
+  type    = string
+  default = "ai-cloud.ru"
+}
