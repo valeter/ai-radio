bd768b38d871c32c02b0e12168d31ae5385fbad6
Ivan Anisimov <ivananisimov2010@gmail.com>
2025-02-09 12:57:18 +0300

    add some permissions to base sa

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -3,6 +3,16 @@ resource "yandex_iam_service_account" "ai-radio-base-sa" {
   description = "Basic service account for ai-radio"
 }
 
+resource "yandex_resourcemanager_folder_iam_member" "ai-radio-base-sa-storage-editor" {
+  role   = "storage.editor"
+  member = "serviceAccount:${yandex_iam_service_account.ai-radio-base-sa.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "ai-radio-base-sa-gw-editor" {
+  role   = "api-gateway.editor"
+  member = "serviceAccount:${yandex_iam_service_account.ai-radio-base-sa.id}"
+}
+
 resource "yandex_iam_service_account_static_access_key" "ai-radio-static-key" {
   service_account_id = yandex_iam_service_account.ai-radio-base-sa.id
 }
