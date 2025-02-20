15b66ab1bbd75a18ac97108885e3ebd776ad3d01
Ivan Anisimov <ivananisimov2010@gmail.com>
2025-02-20 23:31:16 +0300

    change cloud name

--- terraform/cloud.tf
+++ terraform/cloud.tf
@@ -1,34 +1,34 @@
-resource "yandex_resourcemanager_cloud" "ai_radio_prod" {
+resource "yandex_resourcemanager_cloud" "ai-radio-prod" {
   organization_id = var.organization_id
-  name            = "ai_radio_prod"
+  name            = "ai-radio-prod"
 }
 
 resource "yandex_resourcemanager_folder" "network" {
-  cloud_id = yandex_resourcemanager_cloud.ai_radio_prod.id
+  cloud_id = yandex_resourcemanager_cloud.ai-radio-prod.id
   name     = "network"
 }
 
 resource "yandex_resourcemanager_folder" "logs" {
-  cloud_id = yandex_resourcemanager_cloud.ai_radio_prod.id
+  cloud_id = yandex_resourcemanager_cloud.ai-radio-prod.id
   name     = "logs"
 }
 
 resource "yandex_resourcemanager_folder" "sa" {
-  cloud_id = yandex_resourcemanager_cloud.ai_radio_prod.id
+  cloud_id = yandex_resourcemanager_cloud.ai-radio-prod.id
   name     = "sa"
 }
 
 resource "yandex_resourcemanager_folder" "secrets" {
-  cloud_id = yandex_resourcemanager_cloud.ai_radio_prod.id
+  cloud_id = yandex_resourcemanager_cloud.ai-radio-prod.id
   name     = "secrets"
 }
 
 resource "yandex_resourcemanager_folder" "storage" {
-  cloud_id = yandex_resourcemanager_cloud.ai_radio_prod.id
+  cloud_id = yandex_resourcemanager_cloud.ai-radio-prod.id
   name     = "storage"
 }
 
 resource "yandex_resourcemanager_folder" "registry" {
-  cloud_id = yandex_resourcemanager_cloud.ai_radio_prod.id
+  cloud_id = yandex_resourcemanager_cloud.ai-radio-prod.id
   name     = "registry"
 }
