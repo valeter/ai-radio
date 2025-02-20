95fb3bcdaff6624cc8bd4131f965c59dcd6cf613
Ivan Anisimov <ivananisimov2010@gmail.com>
2025-02-20 23:18:08 +0300

    terraform next step

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
     }
-    bucket                      = "ai-cloud-tf-storage"
+    bucket                      = "valter-bootstrap-tf"
     key                         = "terraform.tfstate"
     region                      = "ru-central1"
     skip_region_validation      = true
