e6adc768ff8d3e786fefe988f7e921845e3d767d
Ivan Anisimov <ivananisimov2010@gmail.com>
2025-03-08 19:42:10 +0300

    fix mq type

--- terraform/mq.tf
+++ terraform/mq.tf
@@ -1,5 +1,5 @@
 resource "yandex_message_queue" "ai-radio-voice-gen" {
-  name                       = "ai-radio-voice-gen.fifo"
+  name                       = "ai-radio-voice-gen"
   visibility_timeout_seconds = 600    // 10 min
   message_retention_seconds  = 345600 // 4 days
   max_message_size           = 262144 // 256 kb
@@ -9,22 +9,18 @@ resource "yandex_message_queue" "ai-radio-voice-gen" {
     maxReceiveCount     = 3
   })
   receive_wait_time_seconds   = 2
-  fifo_queue                  = true
-  content_based_deduplication = true
   access_key                  = sensitive(data.yandex_lockbox_secret_version.aws-sa-static-key-version.entries[1].text_value)
   secret_key                  = sensitive(data.yandex_lockbox_secret_version.aws-sa-static-key-version.entries[0].text_value)
 }
 
 resource "yandex_message_queue" "ai-radio-voice-gen-dlq" {
-  name                        = "ai-radio-voice-gen-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
+  name                        = "ai-radio-voice-gen-dlq"
   access_key                  = sensitive(data.yandex_lockbox_secret_version.aws-sa-static-key-version.entries[1].text_value)
   secret_key                  = sensitive(data.yandex_lockbox_secret_version.aws-sa-static-key-version.entries[0].text_value)
 }
 
 data "yandex_message_queue" "ai-radio-voice-gen" {
-  name       = "ai-radio-voice-gen.fifo"
+  name       = "ai-radio-voice-gen"
   access_key = sensitive(data.yandex_lockbox_secret_version.aws-sa-static-key-version.entries[1].text_value)
   secret_key = sensitive(data.yandex_lockbox_secret_version.aws-sa-static-key-version.entries[0].text_value)
 }
