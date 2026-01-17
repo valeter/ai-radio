ad6d72c4755b7bb98bb7b2a61a0e0e6bf9a62ede
Ivan Anisimov <ivananisimov2010@gmail.com>
2026-01-17 17:40:40 +0300

    add provider

---
+++ terraform-k8s/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = var.kubeconfig_path
+  config_context = var.kubeconfig_context
+}
